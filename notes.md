381f9cff4fda5920762c7bb91d936f08cdeaca35
kbongcasmis <kbongcasmis@gmail.com>
2018-01-06 15:44:11 -0600

    found class name for search results and change bg color

---
+++ notes.md
@@ -0,0 +1,4 @@
+
+What i want:
+
+Able to table through search results in google.
---
+++ tab-through.js
@@ -0,0 +1,10 @@
+/**
+ * Changes the display of a google search result and use
+ * tab to cycle through the results.
+ *
+ * @module tab-through.js
+ */
+
+let result_set = document.getElementsByClassName("g");
+result_set[0].style['background-color'] = '#FF00FF';
+
