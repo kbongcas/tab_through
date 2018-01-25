12a6adb11eb124046479a44d210f218dd5a38cf8
kbongcasmis <kbongcasmis@gmail.com>
2018-01-24 20:38:00 -0600

    addes light blue background to focused search results

--- tab-through.js
+++ tab-through.js
@@ -5,8 +5,10 @@
  * @author kbongcas
  */
 
+// results are found using class name "r"
 let result_set = document.getElementsByClassName("r");
 
+// Adds tab index priority to search results
 for (let i = 1; i <= result_set.length; i++) {
     result_set[i-1].getElementsByTagName("a")[0].setAttribute("tabindex", i.toString());
 }
